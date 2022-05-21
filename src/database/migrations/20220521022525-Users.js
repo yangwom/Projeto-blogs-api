6e0a444ecf18e90e4080ee6334956abf5fba4d16
yang vieira <yangflix3@gmail.com>
2022-05-20 22:55:24 -0400

    migrate delete

--- src/database/migrations/20220521022525-Users.js
+++ src/database/migrations/20220521022525-Users.js
@@ -1,21 +1,40 @@
-'use strict';
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('Users',
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+
+        },
+
+        displayName: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+
+        email: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          unique: true,
+        },
+
+        password: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+
+        image: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        }
+
+      });
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+    await queryInterface.dropTable('users');
+  },
 };
