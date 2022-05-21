25b1bcc3394c74977ff38066603d411c2c5f83dd
yang vieira <yangflix3@gmail.com>
2022-05-21 00:16:21 -0400

    campos de migrate BlogPosts

--- src/database/migrations/20220521022938-BlogPosts.js
+++ src/database/migrations/20220521022938-BlogPosts.js
@@ -1,21 +1,47 @@
-'use strict';
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('BlogPosts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+
+      },
+
+      content: {
+        type: Sequelize.STRING,
+      },
+
+      userId: {
+        type: Sequelize.INTEGER,
+        refereces: {
+          model: 'Users',
+          key: 'id',
+        },
+        primaryKey: true,
+      },
+
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'published',
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'updated',
+
+      },
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+    await queryInterface.dropTable('users');
+  },
 };
