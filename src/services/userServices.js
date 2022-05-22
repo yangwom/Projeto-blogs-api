8af5be2e85a94d26da66b593e575c6368b30ab64
yang vieira <yangflix3@gmail.com>
2022-05-22 15:42:25 -0400

    services user

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -3,12 +3,14 @@ const status = require('../status');
 const { User } = require('../database/models');
 const jwtConfig = require('../jwtConfig/jwtConfig');
 
-const userIsValid = async (email) => {
+const userIsValid = async (display, email, password, image) => {
   const user = await User.findOne({ where: { email } });
 
   const exist = user.email === email;
 
   if (exist) throw status.errorConflict;
+
+  await User.create({ display, email, password, image });
 
   const token = jwt.sign({ data: user }, process.env.JWT_SECRET, jwtConfig);
 
