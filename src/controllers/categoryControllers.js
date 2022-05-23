e977ace328df4f483dbff601cb79d9bd4882113a
yang vieira <yangflix3@gmail.com>
2022-05-23 00:54:15 -0400

    category controllers

---
+++ src/controllers/categoryControllers.js
@@ -0,0 +1,16 @@
+const status = require('../status');
+const services = require('../services/categoryServices');
+
+const create = async (req, res, next) => {
+  const { name } = req.body;
+  try {
+    const data = await services.create(name);
+    return res.status(status.success).json(data);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {
+  create,
+};
