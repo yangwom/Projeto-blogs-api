557f2671ec9523d7f4784eb1beb99c07aff83396
yang vieira <yangflix3@gmail.com>
2022-05-24 17:42:15 -0400

    post category deleted

--- src/routers/postCategory/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { authMiddleware } = require('../../middleware/authMiddleware');
-const categoryMiddleware = require('../../middleware/categoryMiddleware');
-
-const routerPostCategory = express.Router();
-
-routerPostCategory.post('/', authMiddleware, categoryMiddleware);
