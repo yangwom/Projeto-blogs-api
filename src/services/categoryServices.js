7381c8e119f394233ad53076710388a7e3ee8a0c
yang vieira <yangflix3@gmail.com>
2022-05-24 23:02:06 -0400

    post category

--- src/services/categoryServices.js
+++ src/services/categoryServices.js
@@ -3,10 +3,7 @@ const { Category } = require('../database/models');
 const create = async (name) => {
   const data = await Category.create({ name });
 
-  return {
-    id: data.null,
-    name: data.name,
-  };
+  return data;
 };
 
 const getAll = async () => {
