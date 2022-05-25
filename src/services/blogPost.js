e6269397f606fecbf05b591b10d6aafb166fea23
yang vieira <yangflix3@gmail.com>
2022-05-24 21:26:47 -0400

    renomeando variaves

--- src/services/blogPost.js
+++ src/services/blogPost.js
@@ -2,14 +2,14 @@ const { BlogPost, Category } = require('../database/models');
 const status = require('../status');
 
 const create = async (title, content, userId, categoryIds) => {
-const categoryid = await Category.findAll({
+const result = await Category.findAll({
     where: { id: categoryIds },
 });
 
-if (categoryid.length !== categoryIds.length) throw status.categorysIdsNotFound;
+if (!result.length) throw status.categorysIdsNotFound;
 
 const createBlogPost = await BlogPost.create({ title, content, userId });
-createBlogPost.addCategories(categoryid);
+await createBlogPost.addCategory(result);
 return createBlogPost;
 };
 
